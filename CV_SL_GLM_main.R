69b19f9a5c4b4395b535ff5119ce7caf069336b3
Jeremiah Jones <jeremiah_jones@urmc.rochester.edu>
2020-10-05 12:42:48 -0400

    Initial commit

---
+++ CV_SL_GLM_main.R
@@ -0,0 +1,463 @@
+
+rm(list=ls())
+
+library(dplyr)
+library(caret)
+library(foreach)
+library(SuperLearner)
+library(readxl)
+library(doParallel)#Load parallel library
+
+no_cores <- 30 # Set number of cores for parallel computing
+main_data_path <- "./main_analysis_data.csv"
+# output files
+bootstrap_file <- "./results/SL_bootstrap.RData"
+false_neg_pred_file <- "./results/pred_false_negative_point01.csv"
+auc_diff_file <- "./results/main_auc_diff.txt"
+report_table_file <- "./results/main_report_table.csv"
+lr_table_file <- "./results/main_lr_table.csv"
+superiority_file <- "./results/superiority_table.csv"
+auc_plot_file <- "./results/main_auc.png"
+
+
+group_testind <- function(groups, all_idxs, k=10){
+  trfolds <- caret::groupKFold(groups, k=k)
+  lapply(trfolds, function(tr) setdiff(all_idxs, tr))
+}
+
+bs.test.charac<-function(fullDat,bs.idxs, k) {
+  
+  
+  dat <- fullDat[bs.idxs,]
+  these_records <- record_ids[bs.idxs]
+  
+  # set the seed to ensure the folds are created the same
+  set.seed(70889)
+  
+  folds <- group_testind(these_records, seq_len(length(bs.idxs)), k=k)
+  
+  fold_record_ids <- lapply(folds, function(fold) these_records[fold])
+  
+  inner_folds <- lapply(seq_along(folds), function(out.idx){
+    train_folds <- setdiff(1:k, out.idx)
+    fold_pkey <- fold_record_ids[train_folds] %>% do.call(c, .)
+    fold_idxs <- folds[train_folds] %>% do.call(c, .)
+    inner_folds <- group_testind(fold_pkey, seq_len(length(fold_idxs)), k=k)
+    # caret::createFolds(dat$BI[fold_idxs], k=k)
+  })
+  
+  #=====================================================================#
+  #============ step 3: TEST CHARACTERISTICS ========================#
+  #=====================================================================#
+  
+  
+  bs.data.use_nosocial <-dat
+  
+  pred_list <- vector("list", length=k)
+  for(i in 1:k){
+    
+    testIndexes <- folds[[i]]
+    test_nosocial <- bs.data.use_nosocial[testIndexes,]
+    train_nosocial <- bs.data.use_nosocial[-testIndexes,]
+    
+    Y_nosocial <-as.numeric(train_nosocial$BI)
+    X_nosocial<-train_nosocial[,!names(train_nosocial)%in%c("BI")]
+    X_holdout_nosocial <-test_nosocial[,!names(test_nosocial)%in%c("BI")]
+    Y_holdout_nosocial<-as.numeric(test_nosocial$BI)
+    
+    validRows <- inner_folds[[i]]
+    
+    sl_nosocial = SuperLearner(Y = Y_nosocial, X = X_nosocial, family = binomial(), method = "method.AUC",
+                               SL.library = SL.lib,
+                               cvControl = list(shuffle=FALSE, validRows=validRows))
+    
+    
+    glm_nosocial = SuperLearner(Y = Y_nosocial, X = X_nosocial, family = binomial(), method = "method.AUC",
+                                SL.library = c( "SL.glm"),
+                                cvControl = list(shuffle=FALSE, validRows=validRows))
+    
+    ##################SL RESULTS
+    sl_nosocial_pred = predict(sl_nosocial, X_holdout_nosocial, onlySL = T,type="response")$pred
+    
+    ##################glm RESULTS
+    glm_nosocial_pred = predict(glm_nosocial, X_holdout_nosocial, onlySL = T,type="response")$pred
+    
+    pred_list[[i]] <- cbind(
+      `Record ID`=fold_record_ids[[i]],
+      SL=sl_nosocial_pred,
+      GLM=glm_nosocial_pred
+    )
+  }
+  
+  pred_df <- do.call(rbind, pred_list) %>% data.frame
+  
+  list(pred=pred_df)
+}
+
+
+
+
+
+
+
+
+raw <- read.csv(main_data_path,
+                header=T)
+# raw = raw[-1,]
+colnames(raw)[1] <- "Record.ID"
+
+
+raw <- raw %>% mutate(BI=ifelse(is.na(True.Bacterial.Infection), 
+                                0, 
+                                True.Bacterial.Infection))
+
+#==========================================================================================#
+#============================ step 1: data processing =====================================#
+#==========================================================================================#
+##Clean data set
+library(dplyr)
+features.raw <- raw %>%
+  mutate(sex= factor(Sex, labels = c("male", "female")),
+         insurance = factor(Insurance.Type, labels = c("private", "public", "none")),
+         cm_condition = factor(Presence.of.Chronic.Medical.Condition..CMC., labels = c("no", "yes")),
+         age = as.numeric(Age),
+         gestationage = as.numeric(Gestational.Age),
+         appearance = factor(Ill.Appearing, labels = c("well", "ill","unknown")),
+         maxtemp = as.numeric(Maximum.Temperature),
+         numdaysill = as.numeric(Number.of.Days.of.Illness),
+         cough = factor(Cough.Present.During.Illness., labels = c("no", "yes","unknown" )),
+         puti = factor(Positive.Urinary.Tract.Inflammation, labels = c("no", "yes", "unknown")),
+         BI = factor(BI, labels = c("no", "yes"))
+         )
+
+features_nosocial = features.raw %>% select(Record.ID, sex, insurance, cm_condition, age, gestationage, 
+                                            appearance, maxtemp, numdaysill, cough, puti,
+                                            BI)
+
+
+dim(features_nosocial)
+
+##Explore data set
+missingFeatures_nonsocial <- as.data.frame(apply(features_nosocial, 2, function(x) sum(is.na(x))))
+# no missingness
+
+data.features_nosocial <- data.frame(model.matrix(BI ~., data = features_nosocial)[,-1],
+                                     BI=features_nosocial$BI)
+record_ids <- data.features_nosocial[,1]
+
+data.use_nosocial <- data.features_nosocial[-1]
+
+data.use_nosocial$BI<-as.numeric(data.use_nosocial$BI)-1
+
+
+
+
+nb<-500
+bs.dat <- vector("list", length = nb)
+
+set.seed(608991)
+for(i in 1:nb) {
+  idxs <- sample(nrow(data.use_nosocial),replace=TRUE)
+  bs.dat[[i]]<-idxs
+  
+}
+
+Y_nosocial <-as.numeric(data.use_nosocial$BI)
+X_nosocial<-data.use_nosocial[,!names(data.use_nosocial)%in%c("BI")]
+
+# learner=create.Learner("SL.randomForest", tune = list(mtry = seq(6,29,1),ntrees=c(500,1000)))	
+
+rflrn=create.Learner("SL.randomForest", 
+                     tune = list(mtry = seq(6,13,2),
+                                 ntrees=c(1000)))
+
+SL.lib <- c(
+  "SL.earth",
+  "SL.gam",
+  "SL.glm",
+  "SL.glmnet",
+  rflrn$names
+)
+
+sl_nosocial = CV.SuperLearner(
+  Y = Y_nosocial,
+  X = X_nosocial,
+  family = binomial(),
+  method = "method.AUC",
+  SL.library = SL.lib
+)
+
+glm_nosocial = CV.SuperLearner(
+  Y = Y_nosocial,
+  X = X_nosocial,
+  family = binomial(),
+  method = "method.AUC",
+  SL.library = c("SL.glm")
+)
+
+full_data_pred_df <- data.frame(Y=Y_nosocial,
+                                SL=sl_nosocial$SL.predict,
+                                GLM=glm_nosocial$SL.predict)
+
+cl <- makeCluster(no_cores, type="FORK", outfile="cluster.out")
+registerDoParallel(cl)
+
+results.list <- foreach(k=1:nb, 
+                        .packages = c("SuperLearner","MASS","caret",
+                                      "randomForest","dplyr","glmnet")
+                        ) %dopar%  
+  tryCatch(
+    bs.test.charac(fullDat=data.use_nosocial, 
+                   bs.idxs=bs.dat[[k]],
+                   k=10),
+    error = function(e) {
+      print(e)
+      NA
+    }
+  )
+
+stopCluster(cl)
+
+save(results.list, raw, nb,
+     full_data_pred_df,
+     file=bootstrap_file)
+
+
+################################################################
+# To inspect the results, load the file and run the code below
+################################################################
+library(caret)
+library(dplyr)
+library(ggplot2)
+
+full_data_pred_df %>% 
+  mutate(id=record_ids) %>% 
+  filter(Y==1 & (SL < .01 | GLM < .01)) %>% 
+  rename(LR = GLM) %>%
+  mutate(LR = round(LR, 3),
+         SL = round(SL,3)) %>% 
+  select(id, SL, LR) %>% 
+  write.csv(file=false_neg_pred_file, row.names=FALSE)
+
+# full_data_pred_df %>%
+#   mutate(id=record_ids) %>%
+#   filter(Y==1 & (SL < .05 | GLM < .05)) %>%
+#   rename(LR = GLM) %>%
+#   mutate(LR = round(LR, 3),
+#          SL = round(SL,3)) %>%
+#   select(id, SL, LR) %>%
+#   write.csv(file="pred_false_negative_point05.csv", row.names=FALSE)
+# 
+# full_data_pred_df %>%
+#   mutate(id=record_ids) %>%
+#   filter(Y==1 & (SL < .03 | GLM < .03)) %>%
+#   rename(LR = GLM) %>%
+#   mutate(LR = round(LR, 3),
+#          SL = round(SL,3)) %>%
+#   select(id, SL, LR) %>%
+#   write.csv(file="pred_false_negative_point03.csv", row.names=FALSE)
+
+### ROC Curves
+library(pROC)
+sl_roc = pROC::roc(Y~SL, data=full_data_pred_df)
+lr_roc = pROC::roc(Y~GLM, data=full_data_pred_df)
+rrc_roc = pROC::roc(response=raw$BI, predictor=raw$RLR)
+
+auc_legend = sapply(1:3, function(i) {
+  if(i == 1){
+    roc = sl_roc
+    name_str = "SL"
+  } else if(i == 2){
+    roc = lr_roc
+    name_str = "LR"
+  } else {
+    roc = rrc_roc
+    name_str = "RLR"
+  }
+  ci = ci.auc(roc)
+  lims = round(ci[-2],3)
+  auc = round(ci[2],3)
+  
+  paste0(name_str, " AUC: ", auc, " CI: (", lims[1], ", ", lims[2], ")")
+})
+png(auc_plot_file, width=600, height=600, pointsize = 16)
+plot(rrc_roc, col="black", lty=3,
+     xlab="Specificity (1 - False Positive %)",
+     ylab="Sensitivity (True Positive %)")
+plot(sl_roc, col="blue", lty=1,
+     add=T)
+plot(lr_roc, col="red", lty=5,
+     add=T)
+legend(x=0.65, y=0.15,
+       legend=auc_legend,
+       col=c("blue", "red", "black"),
+       lty=c(1,5,3),
+       cex=0.9)
+dev.off()
+
+
+joined.results.list <- lapply(seq_along(results.list), function(i) {
+  x <- results.list[[i]]
+  pred <- x$pred
+  colnames(pred) <- c("Record.ID", "SL", "GLM")
+  joined_pred <- raw %>% select(BI, RLR, Record.ID) %>%
+    inner_join(pred, by="Record.ID")
+  stopifnot(nrow(joined_pred) == nrow(pred))
+  return(joined_pred)
+})
+
+# bootstrapped confusion matrix statistics
+nb <- 500
+conf_stat_names <- c("Sensitivity", "Specificity", "Pos Pred Value", "Neg Pred Value")
+cut.pts <- c(.01, .03, .05)
+
+no_cores <- 2 #Set number of cores
+cl <- makeCluster(no_cores)
+registerDoParallel((cl))
+tbl <- foreach(cut.point=cut.pts, .export="joined.results.list",
+               .packages = c("caret","dplyr")
+) %dopar% {
+  confusion <- sapply(1:nb, function(i){
+    boot_df <- joined.results.list[[i]]
+    
+    sl_conf <- boot_df %>%
+      select(SL, BI) %>%
+      mutate(SL=1*(SL > cut.point)) %>%
+      table %>%
+      caret::confusionMatrix(positive="1")
+    sl_conf <- sl_conf$byClass[conf_stat_names]
+    
+    glm_conf <- boot_df %>%
+      select(GLM, BI) %>%
+      mutate(GLM=1*(GLM > cut.point)) %>%
+      table %>%
+      caret::confusionMatrix(positive="1")
+    glm_conf <- glm_conf$byClass[conf_stat_names]
+    
+    naive_conf <- boot_df %>%
+      select(RLR, BI) %>%
+      table %>% 
+      caret::confusionMatrix(positive="1")
+    naive_conf <- naive_conf$byClass[conf_stat_names]
+    
+    diff_sl_conf <- sl_conf - naive_conf
+    diff_glm_conf <- glm_conf - naive_conf
+    
+    ret <- c(sl_conf, glm_conf,
+             diff_sl_conf, diff_glm_conf)
+    names(ret) <- lapply(c("SL.", "GLM.", "DIFF.SL.", "DIFF.GLM."),
+                         paste0, conf_stat_names) %>% do.call(c, .)
+    ret
+  })
+}
+stopCluster(cl)
+
+sl_glm_report_table <- foreach(j=seq_along(cut.pts), 
+                               .combine=cbind
+) %do% {
+  this_tbl <- tbl[[j]]
+  sl_glm_stats <- this_tbl[1:8,]
+  
+  # point estimate = bootstrap means
+  point.est <- rowMeans(sl_glm_stats)
+  conf.ints <- apply(sl_glm_stats, 1, quantile, probs=c(0.025, 0.975))
+  
+  ret <- paste0(round(point.est, 3), " (",
+                round(conf.ints[1,], 3), ", ",
+                round(pmin(1, conf.ints[2,]), 3), ")")
+  names(ret) <- names(point.est)
+  ret
+}
+colnames(sl_glm_report_table) <- cut.pts
+write.csv(sl_glm_report_table, file=report_table_file)
+
+### Likelihood Ratio Calculations
+lr_stats <- foreach(j=seq_along(cut.pts), 
+                    .combine=cbind
+) %do% {
+  this_tbl <- tbl[[j]]
+  sl_glm_stats <- this_tbl[1:8,]
+  
+  sl.lrps <- sl_glm_stats["SL.Sensitivity",]/(1-sl_glm_stats["SL.Specificity",])
+  glm.lrps <- sl_glm_stats["GLM.Sensitivity",]/(1-sl_glm_stats["GLM.Specificity",])
+  
+  sl.lrns <- (1-sl_glm_stats["SL.Sensitivity",])/sl_glm_stats["SL.Specificity",]
+  glm.lrns <- (1-sl_glm_stats["GLM.Sensitivity",])/sl_glm_stats["GLM.Specificity",]
+  
+  boot_stats <- rbind(sl.lrps, glm.lrps, sl.lrns, glm.lrns)
+  point.est <- rowMeans(boot_stats)
+  conf.ints <- apply(boot_stats, 1, quantile, probs=c(0.025, 0.975))
+  
+  
+  ret <- paste0(round(point.est, 3), " (",
+                round(conf.ints[1,], 3), ", ",
+                round(pmin(1, conf.ints[2,]), 3), ")")
+  names(ret) <- c("SL.LR+", "GLM.LR+",
+                  "SL.LR-", "GLM.LR-")
+  ret
+}
+colnames(lr_stats) <- cut.pts
+write.csv(lr_stats, file=lr_table_file)
+
+diff_tests <- foreach(j=seq_along(cut.pts), 
+                      .combine=cbind
+) %do% {
+  this_tbl <- tbl[[j]]
+  diff_stats <- this_tbl[c(9:10, 13:14),]
+  
+  # test hypothesis that diff specificity > 0
+  upper_cis <- apply(diff_stats, 1, quantile, probs=0.05)
+  upper_pvals <- apply(diff_stats, 1, function(x) mean(x <= 0))
+  
+  # test hypothesis that diff sensitivity < 0
+  lower_cis <- apply(diff_stats, 1, quantile, probs=0.95)
+  lower_pvals <- apply(diff_stats, 1, function(x) mean(x >= 0))
+  
+  round(c(upper_cis, lower_cis, upper_pvals, lower_pvals), 3)
+}
+colnames(diff_tests) <- cut.pts
+rownames(diff_tests) <- foreach(a=c("Upper.CI.","Lower.CI.","Upper.p.","Lower.p."),
+                                .combine="c") %:%
+  foreach(b=c("SL.", "GLM."),.combine="c") %:%
+  foreach(d=c("Diff.Sens","Diff.Spec"),.combine="c") %do% paste0(a,b,d)
+write.csv(diff_tests, file=superiority_file)
+
+
+#### Diff in AUC
+cl <- makeCluster(no_cores)
+registerDoParallel((cl))
+auc_diffs <- foreach(i=1:nb, .combine=cbind, .packages="pROC") %dopar% {
+  
+  boot_df <- joined.results.list[[i]]
+  label <- factor(boot_df$BI, levels=c(0,1), labels=c("No", "Yes"))
+  score_sl <- boot_df$SL
+  score_glm <- boot_df$GLM
+  score_naive <- boot_df$RLR
+  
+  sl_auc <- pROC::auc(response=label, predictor=score_sl)
+  glm_auc <- pROC::auc(response=label, predictor=score_glm)
+  naive_auc <- pROC::auc(response=label, predictor=score_naive)
+  
+  diff_sl_auc <- sl_auc - naive_auc
+  diff_glm_auc <- glm_auc - naive_auc
+  
+  ret <- c(diff_sl_auc, diff_glm_auc)
+  names(ret) <- c("DIFF.SL.AUC", "DIFF.GLM.AUC")
+  ret
+}
+stopCluster(cl)
+
+auc_diff_means <- rowMeans(auc_diffs)
+auc_diff_cis <- apply(auc_diffs, 1, quantile, probs=c(0.025, 0.975))
+
+auc_diff_results <- paste0(round(auc_diff_means, 3), " (",
+                           round(auc_diff_cis[1,], 3), ", ",
+                           round(pmin(1, auc_diff_cis[2,]), 3), ")")
+names(auc_diff_results) <- c("DIFF.SL.AUC", "DIFF.GLM.AUC")
+capture.output(
+  {
+    print("Improvement (over RLR) in AUC")
+    print(auc_diff_results)
+  },
+  file=auc_diff_file
+)
---
+++ README.md
@@ -0,0 +1,78 @@
+# pediatric-bi-2020
+
+**Disclaimer:** The results presented below have not been externally validated. Do not use the information here for medical decisions.
+
+This project contains all the code and data necessary for reproducing the results of *PUBLISHED PAPER CITATION*. The purpose of this project is to provide a risk calculator for Bacterial Infections among febrile infants. An example of this risk calculator may be found at https://www.urmc.rochester.edu/sites/biriskcalculator/.
+
+# Data Codebook
+
+Information on this data can be found in *PUBLISHED PAPER CITATION*. The two main data files are `main_analysis_data.csv` and `sensitivity_analysis_data.csv`. The codebook for the variables in these files is given below.
+
+Variable Name | Coding | Notes
+--------------|--------|------------
+Record ID| Int | Unique identifier
+Sex|0 = Male, 1 = Female| 
+Insurance Type|0 = Private, 1 = Public,2 = None|
+Presence of Chronic Medical Condition (CMC)|0 = No,1 = Yes|
+Age|Continuous | Days
+Gestational Age|Continuous | Weeks; 37.5 = "Full Term"
+Ill Appearing|0 = No, 1 = Yes, 2 = Unknown|
+Maximum Temperature| Continuous | Celsius
+Number of Days of Illness| Continuous |
+Cough Present During Illness?|0 = Yes, 1 = No, 2 = Unknown|
+Positive Urinary Tract Inflammation|0 = No, 1 = Yes, 2 = Unknown|
+RLR|0 = Low, 1 = High, 2 = Unknown|Rochester Low Risk Criteria; Competing risk stratification technique
+Bacterial Infection|0 = No, 1 = Yes|BI = UTI, Meningitis or bacteremia
+
+The first column is a unique identifier for each infant. The final column represents the outcome being studied. The RLR column represents the risk stratification technique we compare to our classifiers. The remaining variables are predictors.
+
+# Project Workflow
+
+`CV_SL_GLM_main.R` produces the results for the main analysis. Variable importance was then examined by `CV_SL_GLM_vimp.R`. The sensitivity analysis, removing medical history variables, was performed in `CV_SL_GLM_nohist.R`. The model for the web calculator is produced in `web_glm_fit.R`.
+
+Intermediate files and output are available in the `results/` directory.
+
+# Session Info
+
+```r
+> sessionInfo()
+R version 3.5.0 (2018-04-23)
+Platform: x86_64-redhat-linux-gnu (64-bit)
+Running under: CentOS release 6.10 (Final)
+
+Matrix products: default
+BLAS: /usr/lib64/R/lib/libRblas.so
+LAPACK: /usr/lib64/R/lib/libRlapack.so
+
+locale:
+ [1] LC_CTYPE=en_US.UTF-8       LC_NUMERIC=C              
+ [3] LC_TIME=en_US.UTF-8        LC_COLLATE=en_US.UTF-8    
+ [5] LC_MONETARY=en_US.UTF-8    LC_MESSAGES=en_US.UTF-8   
+ [7] LC_PAPER=en_US.UTF-8       LC_NAME=C                 
+ [9] LC_ADDRESS=C               LC_TELEPHONE=C            
+[11] LC_MEASUREMENT=en_US.UTF-8 LC_IDENTIFICATION=C       
+
+attached base packages:
+[1] parallel  stats     graphics  grDevices utils     datasets  methods  
+[8] base     
+
+other attached packages:
+ [1] vimp_2.1.0          pROC_1.15.3         doParallel_1.0.15  
+ [4] iterators_1.0.12    readxl_1.3.1        SuperLearner_2.0-24
+ [7] nnls_1.4            foreach_1.5.0       caret_6.0-81       
+[10] ggplot2_3.3.0       lattice_0.20-41     dplyr_1.0.2        
+
+loaded via a namespace (and not attached):
+ [1] Rcpp_1.0.4.6       cellranger_1.1.0   pillar_1.4.3       compiler_3.5.0    
+ [5] gower_0.1.2        plyr_1.8.6         class_7.3-17       tools_3.5.0       
+ [9] rpart_4.1-15       ipred_0.9-8        lubridate_1.7.9    lifecycle_0.2.0   
+[13] tibble_3.0.1       nlme_3.1-149       gtable_0.3.0       pkgconfig_2.0.3   
+[17] rlang_0.4.7        Matrix_1.2-18      prodlim_2018.04.18 withr_2.2.0       
+[21] stringr_1.4.0      generics_0.0.2     vctrs_0.3.4        recipes_0.1.4     
+[25] stats4_3.5.0       nnet_7.3-14        grid_3.5.0         tidyselect_1.1.0  
+[29] glue_1.4.0         data.table_1.13.0  R6_2.4.1           survival_3.2-3    
+[33] lava_1.6.4         purrr_0.3.4        reshape2_1.4.4     magrittr_1.5      
+[37] splines_3.5.0      MASS_7.3-52        scales_1.0.0       codetools_0.2-16  
+[41] ModelMetrics_1.2.2 ellipsis_0.3.0     timeDate_3043.102  colorspace_1.4-1  
+[45] stringi_1.4.3      munsell_0.5.0      crayon_1.3.4 
+```
